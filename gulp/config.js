c28556f7768b3bea2abf5ecb6815f1685128abd9
JinLang <2484882869@qq.com>
2015-07-19 10:22:33 +0800

    Finsh Loading

---
+++ gulp/config.js
@@ -0,0 +1,34 @@
+var dest = './build',
+    src = './src';
+
+module.exports = {
+  browserSync: {
+    server: {
+      // We're serving the src folder as well
+      // for sass sourcemap linking
+      baseDir: [dest, src]
+    },
+    files: [
+      dest + '/**'
+    ]
+  },
+  css: {
+    src: src + "/css/*.css",
+    dest: dest
+  },
+  markup: {
+    src: src + "/www/**",
+    dest: dest
+  },
+  browserify: {
+    // Enable source maps
+    debug: true,
+    // A separate bundle will be generated for each
+    // bundle config in the list below
+    bundleConfigs: [{
+      entries: src + '/app/app.jsx',
+      dest: dest,
+      outputName: 'app.js'
+    }]
+  }
+};
