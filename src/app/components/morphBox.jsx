f7cfd191cdde00c70df2b9623572121d2f41f0c4
JinLang <2484882869@qq.com>
2015-07-25 01:01:23 +0800

    morphBox effect

--- src/app/components/morphBox.jsx
+++ src/app/components/morphBox.jsx
@@ -4,20 +4,66 @@ var MorphBox = React.createClass({
 	
 	getInitialState() {
 		return {
-			image : "landscape.jpg"
+			left : 0,
+			top : 0,
+			width : 0,
+			image : "img/landscape.jpg",
+			boxClass : "box"
 		};
 	},
 
+	changeOpen() {
+		this.setState({
+			boxClass : "box action open"
+		});
+	},
+
+	changeClose() {
+		this.setState({
+			boxClass : "box"
+		});
+	},
+
+	componentDidMount() {
+	    let button = this.refs.imgPosition.getDOMNode();
+	    let buttonPos = button.getBoundingClientRect();
+	    console.log(buttonPos);
+	    this.setState({
+	      left : buttonPos.left,
+	      top : buttonPos.top
+	    });
+	    window.addEventListener('scroll', this.handleScroll);
+	  },
+
+	  componentWillUnmount() {
+	    window.removeEventListener('scroll', this.handleScroll);
+	  },
+
 	render() {
 
+		var styles = {
+	      left: this.state.left,
+	      top: this.state.top,
+	      backgroundImage: 'url(img/landscape.jpg)'
+	    };
+
 		return (
-			<div className="box">
-				<div className="box-main">
-					<div className="box-img">
-						<img alt="alt text" src={"img/" + this.state.image} />
+			<div className={this.state.boxClass}>
+				<div onClick={this.changeOpen} className="box-main">
+					<div ref="imgPosition" className="box-img">
+						<img alt="alt text" src={this.state.image} />
 					</div>
 					<div className="box-inner">
 						<p>Lorem ipsum dolor sit amet.<br/>Consectetur adipiscing elit.</p>
+					</div>
+				</div>
+				<div style={styles} className="box-content">
+					<div>
+						<div className="content-style-overlay">
+							<span onClick={this.changeClose} className="icon-close">close</span>
+							<h2>About Parsley</h2>
+                			<p>Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.</p>
+                		</div>
 					</div>
 				</div>
 			</div>
